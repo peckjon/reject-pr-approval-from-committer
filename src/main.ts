9a744ef9548a74e28d9ec8965187fb06680d6702
bisera-cognite <66322930+bisera-cognite@users.noreply.github.com>
2020-10-20 14:53:03 +0200

    Approve only dependabot PRs that do not contain commits by other users (#3)

--- src/main.ts
+++ src/main.ts
@@ -10,22 +10,43 @@ async function run() {
   try {
     const token = core.getInput('github-token', { required: true });
 
-    const {
-      actor,
-      payload: { pull_request: pr },
-    } = github.context;
+    const { pull_request: pr } = github.context.payload;
     if (!pr) {
       throw new Error('Event payload missing `pull_request`');
     }
 
-    core.info(`PR #${pr.number} opened from ${actor}`);
+    const client = new github.GitHub(token);
 
-    if (!ALLOWED_NAMES[actor]) {
-      core.info(`PR #${pr.number} is not from an approved source (${actor})`);
-      return;
+    // Get a pull request
+    const { data: pullRequest } = await client.pulls.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: pr.number,
+    });
+
+    // Get creator of pull request
+    const pr_user = pullRequest.user.login;
+
+    core.info(`PR #${pr.number} opened from ${pr_user}`);
+
+    // Get list of commits on a pull request
+    const { data: listCommits } = await client.pulls.listCommits({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: pr.number,
+    });
+
+    // Get all commiters on a pull request
+    for (let commit of listCommits) {
+      // Check if there are commiters other than ALLOWED_NAMES
+      if (!ALLOWED_NAMES[commit.author.login]) {
+        core.info(
+          `Commit ${commit.sha} is not from an approved source (${commit.author.login})`
+        );
+        return;
+      }
     }
 
-    const client = new github.GitHub(token);
     core.debug(`Creating approving review for pull request #${pr.number}`);
     await client.pulls.createReview({
       owner: github.context.repo.owner,
